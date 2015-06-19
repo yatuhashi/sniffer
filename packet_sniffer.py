d1849c70cf88cea267ceb7c925b9de9c084836cf
kei <keyansuiya@gmail.com>
2015-06-20 02:05:37 +0900

    split function

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -2,11 +2,11 @@
 #python ver.3.4
 import sys
 from socket import *
- 
+
 ETH_P_IP = 0x800
 s = socket(PF_PACKET, SOCK_RAW, ETH_P_IP)
-s.bind(("lo", ETH_P_IP))
-watch_port=8000
+s.bind(("wlan0", ETH_P_IP))
+watch_port=80
 
 def dump_head(p):
     print("DATA: ",p)
@@ -20,14 +20,14 @@ def dump_head(p):
     print("DestinationPort : %02x%02x :" % (p[34], p[35]),int("%02x%02x" % (p[34],p[35]),16))
 
 
-def dump_data(p):
+def dump_all_data(p):
     for i in range(0, plen, 2):
-    	try:
-    		sys.stdout.write("%02x%02x  " % (p[i], p[i+1])),
-    		if i % 16 == 14:
-    			print(""),
-    	except:
-    		print("\n")
+        try:
+            sys.stdout.write("%02x%02x  " % (p[i], p[i+1])),
+            if i % 16 == 14:
+                print(""),
+        except:
+            print("\n")
 
 while range(1):
     p = s.recv(2048)
@@ -35,4 +35,6 @@ while range(1):
     if watch_port==int("%02x%02x" % (p[32],p[33]),16) or watch_port==int("%02x%02x" % (p[34],p[35]),16):
         print("\n**----------------------------------------------------------------**")
         dump_head(p)
-        dump_data(p)
+        dump_all_data(p)
+
+
